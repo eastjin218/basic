780f45f17177298ecb5386b2375ba06bbe0512c0
eastjin218 <ohsky0218@naver.com>
2022-11-01 06:38:42 +0000

    tfx_basic

---
+++ modules/common.py
@@ -0,0 +1,12 @@
+IMAGE_TFREC_KEY = "image"
+IMAGE_SHAPE_TFREC_KEY = "image_shape"
+LABEL_TFREC_KEY = "label"
+
+IMAGE_MODEL_KEY = "pixel_values"
+LABEL_MODEL_KEY = "labels"
+
+CONCRETE_INPUT = "pixel_values"
+
+PRETRAIN_CHECKPOINT = "google/vit-base-patch16-224-in21k"
+
+LABELS = ["angular_leaf_spot", "bean_rust", "healthy"]
---
+++ modules/model.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+import absl
+
+def INFO(text:str):
+    absl.logging.info(text)
+
+def build_model(pretrained_checkpoint:str = None):
+    if pretrained_checkpoint:
+        model = tf.keras.models.load_model(pretrained_checkpoint)
+    else:
+        inputs = tf.keras.Input(shape=(224,224,3))
+        img_augmentation = tf.keras.models.Sequential(
+            [
+                tf.keras.layers.RandomRotation(factor=0.15),
+                tf.keras.layers.RandomTranslation(height_factor=0.1, width_factor=0.1),
+                tf.keras.layers.RandomFlip(),
+                tf.keras.layers.RandomContrast(factor=0.1),
+            ],
+            name= 'img_augmentation',
+        )
+        x = img_augmentation(inputs)
+        model = tf.keras.applications.efficientnet.EfficientNetB2(
+                weights='imagenet',
+                input_tensor= x,
+                include_top=False)
+        model.trainable =False
+        x = tf.keras.layers.GlobalAveragePooling2D(name = 'avg_pool')(model.output)
+        x = tf.keras.layers.BatchNormalization()(x)
+        top_dropout_rate = 0.2
+        x = tf.keras.layers.Dropout(top_dropout_rate, name = 'top_dropout')(x)
+        outputs = tf.keras.layers.Dense(6, activation='softmax', name ='pred')(x)
+        model = tf.keras.models.Model(inputs=inputs, outputs=outputs, name='EfficientNet')
+    
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    INFO(model.summary())
+    
+    return model
